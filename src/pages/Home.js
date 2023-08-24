f743994280417fc5d0505753ed7948bd0b07ee7b
jwernethUMD <jwerneth@terpmail.umd.edu>
2023-08-23 23:37:23 -0500

    Made home page look a bit neater

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,12 @@
 function Home() {
     return (
         <>
-            <h1>Welcome to Custom Chess!</h1>
-            <a href="/singleplayer">Click here to play</a>
+            <div className="container">
+                <div className="text-center mx-auto mt-5">
+                    <h1>Welcome to Custom Chess!</h1>
+                    <a href="/singleplayer">Click here to play</a>
+                </div>
+            </div>
         </>
     )
 
