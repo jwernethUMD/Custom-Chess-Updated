dd3afd4daa09a597bbb4699746578512e5082847
jwernethUMD <jwerneth@terpmail.umd.edu>
2023-08-23 20:41:41 -0500

    Added bootstrap navigation bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,29 @@
+import React from "react"
+import { NavLink } from "react-router-dom"
+
 function Header() {
     return (
         <>
-            <h1>
-                hi
-            </h1>
+            <div className="navbar navbar-expand-sm navbar-dark bg-dark px-2">
+                <a className="navbar-brand" href="/">
+                    <img className="d-inline-block align-top" height="30" src={`${process.env.PUBLIC_URL}/piece-images/white-king.png`} alt="king icon" />
+                    Custom Chess
+                </a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#ccNavbar" aria-controls="ccNavbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="ccNavbar">
+                    <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <NavLink activeClassName="active" className="nav-link" to="/" >Home</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink activeClassName="active" className="nav-link" to="/singleplayer">Singleplayer</NavLink>
+                    </li>
+                    </ul>
+                </div>
+            </div>
         </>
     )
 }
