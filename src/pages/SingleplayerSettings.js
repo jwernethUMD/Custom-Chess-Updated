454586fc5b3a4591a96d8173b6a5c5ba5eacc295
jwernethUMD <jwerneth@terpmail.umd.edu>
2023-08-24 00:31:54 -0500

    Added new pages for last commit

---
+++ src/pages/SingleplayerSettings.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+function SingleplayerSettings() {
+    return (
+        <>
+            <Link to="/singleplayer/play">Start game</Link>
+        </>
+    )
+}
+
+export default SingleplayerSettings
