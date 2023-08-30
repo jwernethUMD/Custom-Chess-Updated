798e7cad5ee2c2864113ee7ad2c2ac5c7202f808
jwernethUMD <jwerneth@terpmail.umd.edu>
2023-08-30 02:31:38 -0400

    Cleaned up controlbar file a bit more

--- src/components/ControlBar.js
+++ src/components/ControlBar.js
@@ -1,26 +1,7 @@
 import DrawBtn from "./DrawBtn"
 import React from "react"
-import { useState } from "react"
 
 const ControlBar = (props) => {
-    const [showSettings, setShowSettings] = useState({
-        rook: false,
-        knight: false,
-        bishop: false,
-        queen: false,
-        king: false
-    })
-
-    const [showTutorial, setShowTutorial] = useState(false)
-    
-    let pieces = ["rook", "knight", "bishop", "queen", "king"]
-
-    function showMoveSettings(show, type) {
-        let newSettings = global.structuredClone(showSettings)
-        newSettings[type] = show
-        setShowSettings(newSettings)
-    }
-    
     return (
         <>
             <div className="control-bar">
