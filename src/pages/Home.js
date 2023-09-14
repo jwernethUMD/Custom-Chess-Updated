acee1dbf5549ca44d96732ba0eaa33d9f9d9345d
jwernethUMD <jwerneth@terpmail.umd.edu>
2023-09-13 20:33:54 -0400

    Updated home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,22 @@
+import { Link } from "react-router-dom"
+
 function Home() {
     return (
         <>
             <div className="container">
                 <div className="text-center mx-auto mt-5">
                     <h1>Welcome to Custom Chess!</h1>
-                    <a href="/singleplayer">Click here to play</a>
+                    <hr />
+                    <div className="my-4">
+                        <Link to="/multiplayer" className="btn btn-primary mx-2">Play Multiplayer</Link>
+                        <Link to="/singleplayer" className="btn btn-primary mx-2">Play Singleplayer</Link>
+                    </div>
+                    <div className="my-4">
+                        <Link to="https://jwernethumd.github.io/custom-chess/" className="btn btn-primary">Play Old Version</Link>
+                    </div>
+                    <div className="my-4">
+                        <Link to="https://github.com/jwernethUMD/Custom-Chess-Updated" className="btn btn-primary">View Source Code</Link>
+                    </div>
                 </div>
             </div>
         </>
